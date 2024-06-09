057c90c79d9ce4bbf6b1873060e2cbd37c434c49
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-06-09 07:04:04 +0300

    //header

---
+++ src/Home/Header.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './Header.css';
+import { Link,Outlet  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const HeadImage = () => (
+    <img src="head.png" alt="Head" className='head' />
+  );
+  
+
+
+export const MainPageHeader = () => (
+    <>
+
+  <HeadImage />
+
+  <nav  >
+  <Link to="/" className='hotelforyou'>
+    Hotel for <span style={{ color: 'blueviolet', marginLeft: '5px' }}> you.</span>
+</Link>
+
+    
+    <div>
+      <img src="language.png" width="40" alt="Language" className='language'/>
+      <Link to="/register" className='register'>Register</Link>
+      <Link to="/sign_in" className='signin'>Sign In<img src="signin.png" width="40" alt="SignIn" className="signin-icon" /></Link>
+    </div>
+   
+  </nav>
+
+  <Outlet/>
+
+  </>
+
+);
+
+
+
+export  function HotelForYou(){//HOME page 
+    return(
+        <div></div>
+    
+) ;
+}
+
+
+export  function Register(){//ModalDialog Register
+    return(
+        <div></div>
+    
+) ;
+}
+
+
+export  function SignIn(){//ModalDialog SignIn
+    return(
+        <div></div>
+    
+) ;
+}
+
+
+
+export function NotFound(){
+    return(
+        <div></div>
+) ;
+}
+
+
+export function Header (props)
+ {
+  return (
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<MainPageHeader/> } >
+  <Route index element={<HotelForYou />} />
+   <Route path="register" element={<Register/> } />
+  <Route path="sign_in" element={<SignIn />} />
+  <Route path="*" element= {<NotFound />} />  
+
+  </Route>
+</Routes>
+</BrowserRouter>
+
+
+  );
+}
