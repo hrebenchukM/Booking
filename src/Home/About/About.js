ede5971b530ecec81567e3579f9a89e83dfcba60
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-06-20 09:38:19 +0300

    /\

--- src/Home/About/About.js
+++ src/Home/About/About.js
@@ -1,57 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './About.css';
+import AboutArr from './About.json';
+
+export function About() {
 
 
-
-export  function About(){
-  return(
-      <div>
-         <h2 className="header1">SAFE WITH US</h2>
-        <div className='rectangle'style={{ left: '10%' }}>
+  return (
+    <div>
+      <h2 className="header1">SAFE WITH US</h2>
+      {AboutArr.map((item, index) => (
+        <div className='rectangle' style={{ left: item.left }} key={index}>
           <div className='cont'>
-             <img src="about1.png" width={30} className='icon1'></img>
-             <p className='textAbout'>
-             We choose hotels that  meet our standards 
-             </p>
+            <img src={item.imgSrc} width={30} className={item.imgClass}></img>
+            <p className='textAbout'>{item.text}</p>
           </div>
         </div>
-
-        <div className='rectangle' style={{ left: '26%' }}>
-          <div className='cont'>
-             <img src="about2.png" width={30} className='icon2'></img>
-             <p className='textAbout'>
-             Secure payment without hidden fees
-             </p>
-          </div>
-        </div>
-
-        <div className='rectangle'style={{ left: '42%' }}>
-          <div className='cont'>
-             <img src="about3.png" width={30} className='icon3'></img>
-             <p className='textAbout'>
-             Сurrent prices and best deals
-             </p>
-          </div>
-        </div>
-
-        <div className='rectangle'style={{ left: '58%' }}>
-          <div className='cont'>
-             <img src="about4.png" width={30} className='icon4'></img>
-             <p className='textAbout'>
-             Easy booking which will not take you long
-             </p>
-          </div>
-        </div>
-
-
-        <div className='rectangle'style={{ left: '74%' }}>
-          <div className='cont'>
-             <img src="about5.png" width={30} className='icon5'></img>
-             <p className='textAbout'>
-             We'll promptly email  booking confirmation.
-             </p>
-          </div>
-        </div>
-      </div>
-) ;
+      ))}
+    </div>
+  );
 }
--- src/Home/Info/info.js
+++ src/Home/Info/info.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import './info.css';
+import infoArr from './Info.json';
 
+export function Info() {
 
-export  function Info (){
-    return (
-        <div >
-          <h2 className="header2">BE OUR REGULAR</h2>
-            <div className="image">
- </div>
-
-            <div className="text-cont">
-              <div className='textinfo'>
-              <p>
-                We believe that every customer deserves the best, and we're committed to providing top-class services to all of our clients. 
-                When you book with us, you can enjoy not only great deals on your travel arrangements, but also exclusive discounts and special offers. 
-                We value your loyalty and want to show our appreciation by giving back.
-                
-              </p>
-              <p>
-                So start your search today and discover the amazing rewards waiting for you on our website!
-              </p>
-              </div>
-            </div>
+  return (
+    <div>
+      <h2 className="header2">{infoArr.header}</h2>
+      <div className={infoArr.imageClass}></div>
+      <div className={infoArr.textClass}>
+        <div className='textinfo'>
+          {infoArr.paragraphs.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
         </div>
-      );
-    }
-    
+      </div>
+    </div>
+  );
+}
