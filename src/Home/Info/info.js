5dfa8ad99892c627ca3e0a742af78edbe2d97fd8
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-06-27 03:44:40 +0300

    /

--- src/Home/Info/info.js
+++ src/Home/Info/info.js
@@ -3,9 +3,8 @@ import './info.css';
 import infoArr from './Info.json';
 
 export function Info() {
-
   return (
-    <div>
+    <div className="info-container">
       <h2 className="header2">{infoArr.header}</h2>
       <div className={infoArr.imageClass}></div>
       <div className={infoArr.textClass}>
