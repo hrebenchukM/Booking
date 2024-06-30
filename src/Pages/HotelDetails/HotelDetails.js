a758fa84328f9bff5a6794f3984a8c7ddb70d879
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-06-30 07:19:32 +0300

    Перестановочка

--- src/Pages/HotelDetails/HotelDetails.js
+++ src/Pages/HotelDetails/HotelDetails.js
@@ -1,6 +1,6 @@
-import { Footer } from "../../Components/Footer/Footer";
-import { HeadImage } from "../../Components/HeadImg/HeadImg";
-import { Header } from "../../Components/Header/Header";
+import { Footer } from "../../Components/Common/Footer/Footer";
+import { HeadImage } from "../../Components/Home/HeadImg/HeadImg";
+import { Header } from "../../Components/Common/Header/Header";
 import "./HotelDetails.css";
 
 export  function HotelDetailsPage() {
--- src/Pages/SearchResult/SearchResults.js
+++ src/Pages/SearchResult/SearchResults.js
@@ -1,9 +1,9 @@
-import { Footer } from "../../Components/Footer/Footer";
-import { HeadImage } from "../../Components/HeadImg/HeadImg";
-import { HeadImage2 } from "../../Components/HeadImg2/HeadImg";
-import { Header } from "../../Components/Header/Header";
-import { NavS } from "../../Components/Nav2/Nav2";
-import { SearchBar } from "../../Components/Searcher/Searcher";
+import { Footer } from "../../Components/Common/Footer/Footer";
+import { HeadImage } from "../../Components/Home/HeadImg/HeadImg";
+import { HeadImage2 } from "../../Components/SearchResult/HeadImg2/HeadImg";
+import { Header } from "../../Components/Common/Header/Header";
+import { NavS } from "../../Components/SearchResult/Nav2/Nav2";
+import { SearchBar } from "../../Components/Common/Searcher/Searcher";
 
 export  function SearchResultPage() {
     return (
