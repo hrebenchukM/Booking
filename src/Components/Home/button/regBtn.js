950cee302644739e91eff283fc97c7ee343a20f8
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-07-07 07:58:57 +0300

    ///

--- src/Components/Home/button/regBtn.js
+++ src/Components/Home/button/regBtn.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import './regBtn.css'; // Подключение стилей
+import './regBtn.css'; 
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Register } from '../../Modal/Register/Register';
+
 
 export  function  RegBtn (){
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleRegisterClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
   
+  const [openModal1, setOpenModal1] = useState(false);
+
+  const handleClick1 = () => {
+    setOpenModal1(true);
+  };
+
+
+
+
+
     return (
         <div>
             <div className='login-container'>
         {/* <img src="12345.png" width={30} className='icon-flag'></img> */}
            <img src="/Home/regBtn/12345.png" width={30} className='icon-flag'></img>
         
-
-            <Link
-              to="/register"
-              style={{ textDecoration: "inherit", color: "inherit" }}
-            > <button className="login-button" onClick={handleRegisterClick}>
+         
+            <button className="login-button"  onClick={handleClick1}>
             <p className='txt'>Register an account</p>
-          </button></Link>
+          </button>
        
         {/* <img src="6789.png" width={30} className='icon-flagg'></img> */}
             <img src="/Home/regBtn/6789.png" width={30} className='icon-flagg'></img>
       </div>
+      {openModal1 && <Register setOpen={setOpenModal1} />}
         </div>
     );
 };
