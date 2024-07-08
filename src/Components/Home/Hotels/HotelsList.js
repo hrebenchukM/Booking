212d08611d4d9c0f3fd744211bfc084dd9011af4
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-07-08 12:56:36 +0300

    //

--- src/Components/Home/Hotels/HotelsList.js
+++ src/Components/Home/Hotels/HotelsList.js
@@ -1,34 +1,56 @@
 import React from "react";
 import hotelsArr from './hotels.json';
-import { Link,Outlet  } from 'react-router-dom';
-import './HotelStyles2.css';
 import { HotelCard } from "./HotelCard2.js";
+import './HotelStyles2.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export const HotelsList = () => (
- 
-  <section className="hotels">
-            <div className="container1">
-            <div className="hotels__list">
-                {hotelsArr.map((hotel, index) => (
-                   
-                 
-                   <HotelCard
-                     key={index}
-                     id={hotel.id}
-                     imagesArr={hotel.imagesArr}
-                     starsNum={hotel.starsNum}
-                     hotelName={hotel.hotelName}
-                     cityName={hotel.cityName}
-                     countryName={hotel.countryName}
-                     distanceFromCenter={hotel.distanceFromCenter}
-                     nightCost={hotel.nightCost}
-                   />
-                
-                   
-                ))}
-            </div>
-            </div>
-        </section>
 
-);
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
+import 'swiper/css';
+
+
+
+export const HotelsList = () => {
+
+  return (
+    <section className="hotels">
+      <div className="container1">
+        <div  className="hotels__list swiper-container">
+        <Swiper spaceBetween={20}>
+            {hotelsArr.map((hotel, index) => (
+              <SwiperSlide key={index} className="swiper-slide">
+                <HotelCard
+                  id={hotel.id}
+                  imagesArr={hotel.imagesArr}
+                  starsNum={hotel.starsNum}
+                  hotelName={hotel.hotelName}
+                  cityName={hotel.cityName}
+                  countryName={hotel.countryName}
+                  distanceFromCenter={hotel.distanceFromCenter}
+                  nightCost={hotel.nightCost}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+          <div className="hotels__list hotels__list--desktop">
+            {hotelsArr.map((hotel, index) => (
+              <HotelCard
+                key={index}
+                id={hotel.id}
+                imagesArr={hotel.imagesArr}
+                starsNum={hotel.starsNum}
+                hotelName={hotel.hotelName}
+                cityName={hotel.cityName}
+                countryName={hotel.countryName}
+                distanceFromCenter={hotel.distanceFromCenter}
+                nightCost={hotel.nightCost}
+              />
+            ))}
+          </div>
+      </div>
+    </section>
+  );
+};
--- src/Components/Home/Reviews/Reviews.js
+++ src/Components/Home/Reviews/Reviews.js
@@ -2,28 +2,48 @@ import React from "react";
 import reviewsArr from './reviews.json';
 import { ReviewCard } from "./ReviewCard";
 import './ReviewsStyles2.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-export const Reviews = ({
 
-}) => {
+export const Reviews = () => {
     return (
         <section className="reviews">
-        <div className="container1">
-            <h3 className="reviews__title">reviews</h3>
-            <div className="reviews__list">
-            {reviewsArr.map((review, index) => (
-                <ReviewCard
-                    key={index}
-                    avatar={review.avatar}
-                    reviewerName={review.reviewerName}
-                    hotelName={review.hotelName}
-                    daysAgoNum={review.daysAgoNum}
-                    text={review.text}
-                />)
-            )}
-        </div>
-        </div>
-    </section>
-       
-    )
-}
+            <div className="container1">
+                <h3 className="reviews__title">Reviews</h3>
+
+
+                <div className="reviews__list swiper-container">
+                    <Swiper spaceBetween={20}  >
+                        {reviewsArr.map((review, index) => (
+                            <SwiperSlide key={index} className="swiper-slide">
+                                <ReviewCard
+                                    avatar={review.avatar}
+                                    reviewerName={review.reviewerName}
+                                    hotelName={review.hotelName}
+                                    daysAgoNum={review.daysAgoNum}
+                                    text={review.text}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+
+                <div className="reviews__list reviews__list--desktop">
+                    {reviewsArr.map((review, index) => (
+                        <ReviewCard
+                            key={index}
+                            avatar={review.avatar}
+                            reviewerName={review.reviewerName}
+                            hotelName={review.hotelName}
+                            daysAgoNum={review.daysAgoNum}
+                            text={review.text}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
