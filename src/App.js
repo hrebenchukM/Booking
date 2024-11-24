91b4632f737ff0526a7b0234cc076880dd91b221
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-11-24 04:07:16 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { HomePage } from './Pages/Home/Home';
 import { SearchResultPage } from './Pages/SearchResult/SearchResults';
@@ -6,12 +6,14 @@ import { HotelDetailsPage } from './Pages/HotelDetails/HotelDetails';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ErrorPage } from './Pages/Error/Error';
 import { MyProfile } from './Pages/MyProfile/MyProfile';
-
+import { Admin } from './Admin/Admin';
 
 function App() {
 
+   
   return (
     <div className="App">
+     
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -19,6 +21,7 @@ function App() {
         <Route path="/hotels" element={<SearchResultPage />} />
         <Route path="/hotels/:id" element={<HotelDetailsPage />} />
          <Route path="my_profile" element={<MyProfile/> } />
+         <Route path="admin" element={<Admin/> } />
          <Route path="*" element= {<ErrorPage />} />  
       </Routes>
      
